8faeff47af58ae60d8a1f36602410be96fbf64d6
C2Chandelier <charlelie.chandelier@epitech.eu>
2022-09-16 18:32:09 +0200

    fonction tri OK

--- push_swap.php
+++ push_swap.php
@@ -21,6 +21,7 @@ class push_swap
 
             $this->la[0] = $index1;
             $this->la[1] = $index0;
+            echo "sa ";
         } else {
             return;
         }
@@ -34,6 +35,7 @@ class push_swap
 
             $this->lb[0] = $index1;
             $this->lb[1] = $index0;
+            echo "sb ";
         } else {
             return;
         }
@@ -43,6 +45,7 @@ class push_swap
     {
         $this->sa();
         $this->sb();
+        echo "sc ";
     }
 
     public function pa()
@@ -50,6 +53,8 @@ class push_swap
         if (count($this->lb) > 0) {
             $index0 = $this->lb[0];
             array_unshift($this->la, $index0);
+            array_shift($this->lb);
+            echo "pa ";
         } else {
             return;
         }
@@ -60,6 +65,8 @@ class push_swap
         if (count($this->la) > 0) {
             $index0 = $this->la[0];
             array_unshift($this->lb, $index0);
+            array_shift($this->la);
+            echo "pb ";
         } else {
             return;
         }
@@ -71,6 +78,7 @@ class push_swap
             $index0 = $this->la[0];
             array_push($this->la, $index0);
             array_shift($this->la);
+            echo "ra ";
         } else {
             return;
         }
@@ -82,15 +90,17 @@ class push_swap
             $index0 = $this->lb[0];
             array_push($this->lb, $index0);
             array_shift($this->lb);
+            echo "rb ";
         } else {
             return;
         }
     }
 
-    public function rc()
+    public function rr()
     {
         $this->ra();
         $this->rb();
+        echo "rr ";
     }
 
     public function rra()
@@ -99,6 +109,7 @@ class push_swap
             $last = count($this->la) - 1;
             array_unshift($this->la, $this->la[$last]);
             array_pop($this->la);
+            echo "rra ";
         } else {
             return;
         }
@@ -110,23 +121,67 @@ class push_swap
             $last = count($this->lb) - 1;
             array_unshift($this->lb, $this->lb[$last]);
             array_pop($this->lb);
+            echo "rrb ";
         } else {
             return;
         }
     }
 
-    public function rrc()
+    public function rrr()
     {
         $this->rra();
         $this->rrb();
+        echo "rrr ";
     }
 
-    public function vardu(){
+    public function tri()
+    {
+        if (count($this->la) > 1) {
+
+            while (!empty($this->la)) {
+                $min = min($this->la);
+                $sorted = $this->la;
+                sort($sorted);
+                if (isset($this->la[1]) && $this->la[1] < $this->la[0]) {
+                    $this->sa();
+                }
+                if ($this->la === $sorted) {
+                    if (!empty($this->lb)) {
+                        $this->pa();
+                    } else {
+                        return;
+                    }
+                } else {
+                    if ($min != $this->la[0]) {
+                        $this->rra();
+                    } else {
+                        $this->pb();
+                    }
+                }
+            }
+        }
+    }
+
+    public function vardu()
+    {
         var_dump($this->la);
-        //var_dump($this->lb);
+        var_dump($this->lb);
     }
 }
 
-$test = new push_swap();
-$test->rra();
-$test->vardu();
+/* $test = new push_swap();
+$test->tri();
+$test->vardu(); */
+
+array_shift($argv);
+$array = $argv;
+$test = 0;
+while($test < count($array) -1){
+    if($array[$test] < $array[$test+1]){
+        $test++;
+    }
+    else{
+        return;
+    }
+}
+
